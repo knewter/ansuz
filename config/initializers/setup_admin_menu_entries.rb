f20e0896809372f0bc2c00e3d71f90d705c4e24c
Josh Adams <jadams@goro.(none)>
2008-11-14 14:03:07 -0600

    added themes top level menu entry in admin

--- config/initializers/setup_admin_menu_entries.rb
+++ config/initializers/setup_admin_menu_entries.rb
@@ -1,5 +1,9 @@
-Ansuz::PluginManagerInstance.register_admin_menu_entry "Ansuz", 'Choose a Theme',  '/admin/site_settings/choose_theme'
+Ansuz::PluginManagerInstance.add_top_level_menu_entry "Themes"
+
+Ansuz::PluginManagerInstance.register_admin_menu_entry "Themes", 'Choose a Theme',  '/admin/site_settings/choose_theme'
+
 Ansuz::PluginManagerInstance.register_admin_menu_entry "Content", 'Manage Pages',  '/admin/pages'
+
 Ansuz::PluginManagerInstance.register_admin_menu_entry "Ansuz", 'Site Settings', '/admin/site_settings'
 Ansuz::PluginManagerInstance.register_admin_menu_entry "Ansuz", 'List Plugins',  '/admin/plugins'
 Ansuz::PluginManagerInstance.register_admin_menu_entry "Ansuz", 'Logout', '/admin/account/logout'
--- vendor/plugins/ansuz_theme_repository/init.rb
+++ vendor/plugins/ansuz_theme_repository/init.rb
@@ -1,3 +1,3 @@
 # Include hook code here
 require 'ansuz_theme'
-Ansuz::PluginManagerInstance.register_admin_menu_entry('Ansuz', "Administer this ansuz installation's Theme Repository", '/admin/ansuz_themes')
+Ansuz::PluginManagerInstance.register_admin_menu_entry('Themes', "Administer this ansuz installation's Theme Repository", '/admin/ansuz_themes')
