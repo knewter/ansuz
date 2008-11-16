a0049bb22bf3c0be5ad38302ad3b54368b572053
Josh Adams <jadams@goro.(none)>
2008-11-16 17:43:36 -0600

    made menu entries a bit nicer (can click the note now)

--- app/helpers/tabs_helper.rb
+++ app/helpers/tabs_helper.rb
@@ -1,15 +1,15 @@
 module TabsHelper
   def create_tab(tab_name, url, options={})
     span_options = options.delete(:span_options)
+    if span_options && span_options[:note]
+      tab_name << '<span class="note">' + span_options[:note] + "</span>"
+    end
     if !current_page?(url)
       output = link_to(tab_name, url, options)
     else
       output = '<span>'
       output << tab_name
       output << '</span>'
-    end
-    if span_options && span_options[:note]
-      output << '<span class="note">' + span_options[:note] + "</span>"
     end
     output
   end
--- config/initializers/setup_admin_menu_entries.rb
+++ config/initializers/setup_admin_menu_entries.rb
@@ -1,9 +1,9 @@
-Ansuz::PluginManagerInstance.register_admin_menu_entry "Themes", 'Choose a Theme',  '/admin/site_settings/choose_theme'
+Ansuz::PluginManagerInstance.register_admin_menu_entry "Themes", 'Choose a Theme',  '/admin/site_settings/choose_theme', :span_options => { :note => "Easily change the look and feel of your site." }
 
-Ansuz::PluginManagerInstance.register_admin_menu_entry "Content", 'Manage Pages',  '/admin/pages'
+Ansuz::PluginManagerInstance.register_admin_menu_entry "Content", 'Manage Pages',  '/admin/pages', :span_options => { :note => "This is the primary content for your site." }
 
 Ansuz::PluginManagerInstance.register_admin_menu_entry "Ansuz", 'Manage Roles',  '/admin/roles'
-Ansuz::PluginManagerInstance.register_admin_menu_entry "Ansuz", 'Site Settings', '/admin/site_settings'
+Ansuz::PluginManagerInstance.register_admin_menu_entry "Ansuz", 'Site Settings', '/admin/site_settings', :span_options => { :note => "Change your site title, etc." }
 Ansuz::PluginManagerInstance.register_admin_menu_entry "Ansuz", 'List Plugins',  '/admin/plugins'
 Ansuz::PluginManagerInstance.register_admin_menu_entry "Ansuz", 'Logout', '/admin/account/logout'
 
