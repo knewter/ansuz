65b8fb43f47eac51ef4b30bb1e70121c42ef8a40
Josh Adams <jadams@goro.(none)>
2008-11-23 14:31:07 -0600

    updated subject for page review mailer

--- app/models/ansuz_mailer.rb
+++ app/models/ansuz_mailer.rb
@@ -1,6 +1,6 @@
 class AnsuzMailer < ActionMailer::Base
   def page_review_notifications(page, sent_at = Time.now)
-    subject    'Ansuz#page_review_notifications'
+    subject    'Someone has submitted a page for review'
     recipients 'knewter@gmail.com'
     from       'knewter@gmail.com'
     sent_on    sent_at
