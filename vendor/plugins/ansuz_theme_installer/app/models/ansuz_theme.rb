0aec1f7c51c8f29d488465e4f03762b46c22df8b
Josh Adams <jadams@goro.(none)>
2008-11-22 21:13:41 -0600

    updated ansuz theme to slugify the name (sort of, needs more work)

--- vendor/plugins/ansuz_theme_installer/app/models/ansuz_theme.rb
+++ vendor/plugins/ansuz_theme_installer/app/models/ansuz_theme.rb
@@ -5,11 +5,16 @@ module Ansuz
     class AnsuzTheme < ActiveRecord::Base
       validates_uniqueness_of :name
 
+      before_save :clean_up_name
       after_save :update_theme_repository_xml
       # FIXME: The theme installer and repository plugins are intermingled and gross
 
       def update_theme_repository_xml
         Ansuz::JAdams::ThemeRepositoryManager.write_file
+      end
+
+      def clean_up_name
+        self.name = self.name.gsub(/ /, '_')
       end
     end
   end
