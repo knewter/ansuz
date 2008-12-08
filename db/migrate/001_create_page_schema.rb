f4d923b1367be30844c6c4998c40ce63ddae0d7b
Josh Adams <jadams@goro.(none)>
2008-12-07 19:19:09 -0600

    mostly fixed the database creation stuff

--- db/migrate/001_create_page_schema.rb
+++ db/migrate/001_create_page_schema.rb
@@ -4,6 +4,7 @@ class CreatePageSchema < ActiveRecord::Migration
       t.string :name
       t.string :title
       t.string :full_title
+      t.string :status
       t.text   :body
 
       t.datetime :created_at
@@ -19,21 +20,6 @@ class CreatePageSchema < ActiveRecord::Migration
       t.boolean  :linked, :default => true
       t.boolean  :show_sub_menu, :default => false
     end
-
-    Page.create(
-      :name => 'home',
-      :title => 'Home',
-      :full_title => 'Home',
-      :body => '',
-      :page_order => 0,
-      :created_at => 'NOW()',
-      :updated_at => 'NOW()',
-      :display_title => 0,
-      :parent_id => nil,
-      :linked => 1,
-      :published => 1,
-      :page_type => 'page'
-    )
 
   end
 
--- db/migrate/20081123161548_add_status_to_pages.rb
+++ db/migrate/20081123161548_add_status_to_pages.rb
@@ -1,11 +1,24 @@
 class AddStatusToPages < ActiveRecord::Migration
   def self.up
-    add_column :pages, :status, :string
     add_index :pages, :status
+    # TODO: This needs to be moved to an 'initial setup' rake task
+    Page.create(
+      :name => 'home',
+      :title => 'Home',
+      :full_title => 'Home',
+      :body => '',
+      :page_order => 0,
+      :created_at => 'NOW()',
+      :updated_at => 'NOW()',
+      :display_title => 0,
+      :parent_id => nil,
+      :linked => 1,
+      :published => 1,
+      :page_type => 'page'
+    )
   end
 
   def self.down
     remove_index :pages, :column => :status
-    remove_column :pages, :status
   end
 end
