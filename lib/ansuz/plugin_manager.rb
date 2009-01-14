2e200049a87810c348444b1ba03f3d6582649e3d
James Cook <james@spike.(none)>
2009-01-13 19:37:49 -0600

    added a test for Page.expired and dropped in a hack to prevent tests from aborting

--- lib/ansuz/plugin_manager.rb
+++ lib/ansuz/plugin_manager.rb
@@ -66,8 +66,9 @@ module Ansuz
 
     protected
     def create_settings(name)
+      return false if SiteSetting.count == 0 # Stupid, mean hack. There's some test that clears site settings and breaks things.
       site_setting = SiteSetting.first
-      unless( site_setting.settings[name])
+      unless( site_setting.nil? && site_setting.first.settings[name])
         site_setting.settings[name] = {}  
         site_setting.save
       end
