f4c436eec8dc9cdefdb24a19f73ae9275080fde4
Adam Dill <adam@isshen.com>
2009-02-16 17:23:24 -0500

    added a temporyary fix to make the installer work a bit more, by cehcking to see if the db exists before trying to register the content plugin

--- vendor/plugins/ansuz_content_section/init.rb
+++ vendor/plugins/ansuz_content_section/init.rb
@@ -1,4 +1,7 @@
-Ansuz::PluginManagerInstance.register_plugin(Ansuz::JAdams::ContentSection)
-Ansuz::PluginManagerInstance.add_gem_dependency("RedCloth",  :lib => "redcloth")
-Ansuz::PluginManagerInstance.add_gem_dependency("BlueCloth", :lib => "bluecloth")
-Ansuz::PluginManagerInstance.register_page_type("Content Page", ["Ansuz::JAdams::ContentSection"])
+# FIXME: this unless statement needs to be removed, but without it you cant do rake gems:install, rake db:create:all or run the installer
+unless !ActiveRecord::Base.configurations.blank?
+  Ansuz::PluginManagerInstance.register_plugin(Ansuz::JAdams::ContentSection)
+end  
+  Ansuz::PluginManagerInstance.add_gem_dependency("RedCloth",  :lib => "redcloth")
+  Ansuz::PluginManagerInstance.add_gem_dependency("BlueCloth", :lib => "bluecloth")
+  Ansuz::PluginManagerInstance.register_page_type("Content Page", ["Ansuz::JAdams::ContentSection"])
